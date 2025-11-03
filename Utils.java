d64de2e9c877cd3252691250c53f5fbc294bc70b
llizzza <bondarenkolisa18a@gmail.com>
2025-11-03 08:31:10 +0500

    Update and rename Function.java to Utils.java

--- Utils.java
+++ Utils.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-interface Function<T, P> {
-    P apply(T t);
-}
 class Utils {
     public static <T, P> List<P> mapList(List<T> input, Function<T, P> func) {
         List<P> result = new ArrayList<>();
