60f8c750ff9e139871364ea1816405e659384e94
llizzza <bondarenkolisa18a@gmail.com>
2025-11-03 05:22:55 +0500

    Create Comparable.java

---
+++ Comparable.java
@@ -0,0 +1,26 @@
+public interface Comparable<type> {
+    int сравнить(type item);
+}
+
+class ClassComparable implements Comparable<ClassComparable>{
+    private int item;
+
+    public ClassComparable(int item) {
+        this.item = item;
+    }
+
+    public int сравнить(ClassComparable other) {
+        if (this.item < other.item) {
+            return -1;
+        } else if (this.item > other.item) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(item);
+    }
+}
