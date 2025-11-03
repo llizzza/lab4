74cf605c3105dd2fc787b77e0727b2d39634f94c
llizzza <bondarenkolisa18a@gmail.com>
2025-11-03 08:32:20 +0500

    Update and rename Reduction.java to  Utils3.java

--- Utils3.java
+++ Utils3.java
@@ -1,9 +1,5 @@
 import java.util.List;
 
-interface Reduction<T> {
-    T apply(T a, T b);
-}
-
 class Utils3 {
     public static <T> T reduceList(List<T> list, Reduction<T> reduction, T defaultValue) {
         T result = defaultValue;
