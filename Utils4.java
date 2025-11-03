7761f32d7cc997027d33a65b7ed9621eb95abdfd
llizzza <bondarenkolisa18a@gmail.com>
2025-11-03 08:33:14 +0500

    Update and rename Collector.java to Utils4.java

--- Utils4.java
+++ Utils4.java
@@ -1,13 +1,5 @@
 import java.util.List;
 
-interface Collector<P, T> {
-    void add(P collection, T item);
-}
-
-interface CollectionFactory<P> {
-    P create();
-}
-
 class Utils4 {
     public static <T, P> P collectList(List<T> source, CollectionFactory<P> factory, Collector<P, T> collector) {
         P result = factory.create();
